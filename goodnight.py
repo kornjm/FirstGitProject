cc5e05c941221b6b4047fc31108d0f39b25e77c8
enrica <ahnjm2017@gmail.com>
2022-03-10 11:39:31 +0900

    Added hello world and enhanced good night features

---
+++ goodnight.py
@@ -0,0 +1,5 @@
+print("good night")
+print("and sweet dreams")
+print("modified")
+print("modified 2")
+print("Tommorrow 7 a.m")
--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,3 @@
 print("Hello World!")
 print("Welcome My Friend")
+print("test kornjm")
